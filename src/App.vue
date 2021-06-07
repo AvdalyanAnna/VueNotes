6b579d36a3c456fb957b8c822fe8b1f5aacb91b6
AvdalyanAnna <anna.avdalyan.2017@gmail.com>
2021-06-07 23:06:25 +0400

    add project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wrappeer">
+<!--    <header></header>-->
+  <div class="wrapper-content">
+    <section>
+      <div class="container">
+        <h1>Hello</h1>
+        <img alt="Vue logo" src="./assets/logo.png">
+
+      </div>
+    </section>
+  </div>
+<!--  <footer></footer>-->
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
